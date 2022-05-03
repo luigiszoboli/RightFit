a53d0363248267d433d5563a5c147ac5610b12f9
luigiszoboli <luigi.schmidtzoboli@gmail.com>
2022-05-02 23:03:26 -0300

    Update Front-End Tela do Perfil Complete

---
+++ app/src/main/java/com/luigi/projetc/TelaPerfil.java
@@ -0,0 +1,15 @@
+package com.luigi.projetc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_perfil);
+
+        getSupportActionBar().hide();
+    }
+}
