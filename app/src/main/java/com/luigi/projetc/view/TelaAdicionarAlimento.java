711b5941e176cc15f634537dce5845929af9d131
luigiszoboli <luigi.schmidtzoboli@gmail.com>
2022-05-10 01:01:01 -0300

    simple update

--- app/src/main/java/com/luigi/projetc/view/TelaAdicionarAlimento.java
+++ app/src/main/java/com/luigi/projetc/view/TelaAdicionarAlimento.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TelaAdicionarAliemento extends AppCompatActivity {
+public class TelaAdicionarAlimento extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_adicionar_aliemento);
+        setContentView(R.layout.activity_tela_adicionar_alimento);
     }
 }
