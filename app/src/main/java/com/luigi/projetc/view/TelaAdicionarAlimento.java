d86b8b666f556feba76f275a06bc666ed1ffe434
luigiszoboli <luigi.schmidtzoboli@gmail.com>
2022-05-10 01:03:16 -0300

    simple update

--- app/src/main/java/com/luigi/projetc/view/TelaAdicionarAlimento.java
+++ app/src/main/java/com/luigi/projetc/view/TelaAdicionarAlimento.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.luigi.projetc.R;
+
 public class TelaAdicionarAlimento extends AppCompatActivity {
 
     @Override
