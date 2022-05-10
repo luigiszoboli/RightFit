ac390fd85a981f7c640a3641dc4fa374c6481d8f
luigiszoboli <luigi.schmidtzoboli@gmail.com>
2022-05-10 08:39:58 -0300

    simple update

--- app/src/main/java/com/luigi/projetc/view/TelaRegistroAlimento.java
+++ app/src/main/java/com/luigi/projetc/view/TelaRegistroAlimento.java
@@ -2,7 +2,11 @@ package com.luigi.projetc.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.luigi.projetc.R;
 
 public class TelaRegistroAlimento extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class TelaRegistroAlimento extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_registro_alimento);
+    }
+    public void adicionarAlimento(View v){
+        Intent intent = new Intent(this, TelaAdicionarAlimento.class);
+        intent.putExtra("id_alimente", 0);
+        startActivity(intent);
     }
 }
