1e44898777e8ceaada3fab619009e6ab2cccef62
luigiszoboli <luigi.schmidtzoboli@gmail.com>
2022-05-16 22:07:46 -0300

    update

--- app/src/main/java/com/luigi/projetc/model/Usuario.java
+++ app/src/main/java/com/luigi/projetc/model/Usuario.java
@@ -1,27 +1,16 @@
 package com.luigi.projetc.model;
 
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.luigi.projetc.R;
+import com.luigi.projetc.view.TelaCadastro;
+
 public class Usuario {
-    Integer id;
+
     String nome;
-    String email;
     String senha;
-
-    public Usuario() {
-    }
-
-    public Usuario(Integer id, String nome, String email, String senha) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-    }
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    String email;
 
     public String getNome() {
         return nome;
@@ -29,14 +18,6 @@ public class Usuario {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getSenha() {
@@ -47,5 +28,11 @@ public class Usuario {
         this.senha = senha;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- app/src/main/java/com/luigi/projetc/view/TelaRegistroAlimento.java
+++ app/src/main/java/com/luigi/projetc/view/TelaRegistroAlimento.java
@@ -2,43 +2,15 @@ package com.luigi.projetc.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.luigi.projetc.R;
-import com.luigi.projetc.controller.AlimentoController;
-
-import java.util.ArrayList;
 
 public class TelaRegistroAlimento extends AppCompatActivity {
 
-    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_registro_alimento);
-        listView = findViewById(R.id.listView);
-    }
-    public void adicionarAlimento(View v){
-        Intent intent = new Intent(this, TelaAdicionarAlimento.class);
-        intent.putExtra("id_alimente", 0);
-        startActivity(intent);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        AlimentoController alimentoController = new AlimentoController(this);
-        ArrayList<String> dataset= alimentoController.ListNomesAlimentos();
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                dataset
-        );
-        listView.setAdapter(adapter);
     }
 }
